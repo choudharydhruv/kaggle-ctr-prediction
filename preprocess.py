88d6899e037a2260afcce00e1d5ab5f6e7ea834a
Dhruv choudhary <choudharydhruv@gmail.com>
2014-09-07 14:23:46 -0700

    Final proccessed file

--- preprocess.py
+++ preprocess.py
@@ -10,14 +10,9 @@ import csv
 import pylab as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-def processTrainFeatures(input_file, chunk_size, cat_test_stats, norm_features, gen_stats):
+def processTrainFeatures(input_file, chunk_size, cat_test_stats):
 
     stats = pd.DataFrame()
-    cat_stats = []
-
-    for i in range(0,26):
-	frame = pd.DataFrame()
-	cat_stats.append(frame)
 
     reader = pd.read_csv(input_file, chunksize=chunk_size)
     count = 0 
@@ -35,15 +30,6 @@ def processTrainFeatures(input_file, chunk_size, cat_test_stats, norm_features, 
             stats['count'] = stats[['count', 'count_chunk']].sum(axis=1)
             stats.drop(['sum_chunk', 'count_chunk'], axis=1, inplace=True)
 
-        #Generating histograms for categorical features
-	if gen_stats == 1:
-	    for i in range(0,26):
-	        column_name = "C"+ str(i+1)
-	        cat_view = chunk[['Label',column_name]]
-	        cframe = pd.DataFrame()
-	        cframe['category'] = cat_view.groupby(column_name).size().index
-	        cframe['value'] = cat_view.groupby(column_name).size().values
-                cat_stats[i] = pd.concat([cat_stats[i],cframe])
         '''
         for i in range(0,chunk.shape[0]):
             for e in chunk.iloc[i, 15:41].values:
@@ -54,27 +40,6 @@ def processTrainFeatures(input_file, chunk_size, cat_test_stats, norm_features, 
 	gc.collect()
 
     stats['mean'] = stats['sum'] / stats['count']
-
-    #Finish processing the categorical features	
-    if gen_stats == 1:
-	#Aggregating stats over all chunks
-        for i in range(0,26):
-	    cframe = pd.DataFrame()
-	    cframe['category'] = cat_stats[i].groupby('category').sum().index
-	    cframe['value'] = cat_stats[i].groupby('category').sum().values
-            cat_stats[i] = cframe
-        #Creating a dictionary for categories to find out unique ids in the test set
-        cat_new_stats = []
-        for i in range(0,26):
-	    cl = np.array(cat_stats[i])
-	    cat_dict = {cat[0]:int(cat[1]) for cat in cl}
-	    cat_new_stats.append(cat_dict)
-	    #print len(cat_dict.keys())
-	    #print cat_dict
-	cat_stats = cat_new_stats
-
-    if norm_features == 0:
-        return stats,cat_stats,unique_train_ids;
 
     print("Calculating standard deviation of training integer features.....")
     reader = pd.read_csv(input_file, chunksize=chunk_size)
@@ -104,11 +69,14 @@ def processTrainFeatures(input_file, chunk_size, cat_test_stats, norm_features, 
     tod_ptr = 0
     print tod.shape
 
+    chunk_size_small = chunk_size/2
+
     print("Normalizing integer training features....")
-    reader = pd.read_csv(input_file, chunksize=chunk_size)
-    dtm_reader = pd.read_csv('labelSeries2.csv', chunksize=chunk_size)
+    reader = pd.read_csv(input_file, chunksize=chunk_size_small)
+    #dtm_reader = pd.read_csv('labelSeries2.csv', chunksize=chunk_size_small)
     count = 0 
-    for chunk,dtm_chunk in zip(reader,dtm_reader):
+    for chunk in reader:
+	print 'Writing processed training set:' + str(count * chunk_size_small)
         int_features = chunk[["I"+str(i) for i in range(1,14)]]
         norm_data = (int_features - stats['mean'])/stats['std']
         norm_data = norm_data.fillna(0)
@@ -116,6 +84,20 @@ def processTrainFeatures(input_file, chunk_size, cat_test_stats, norm_features, 
         labels =  chunk[["Id", "Label"]]
         norm_data = pd.concat([labels, norm_data], axis=1)
         #Setting missing categorical values to 00000000 and those training set ids that are not present in test as ffffffff(setB) These ids are not features in the test space, but we use the average behavior in these ids to approximate the average behavior of those ids that are present in test but not in train(setC)
+        cl_new = []
+        for i in range(0,26): 
+            cat_data = chunk[["C"+str(i+1)]]
+            cat_data = cat_data.fillna('00000000')
+	    cl = np.array(cat_data)
+	    cl_col = []
+	    for row in cl:
+	        if row[0] not in cat_test_stats[i] and row[0] != '00000000':
+		    cl_col.append('ffffffff')
+		else:
+		    cl_col.append(row[0])
+	    cl_new.append(np.asarray(cl_col))
+        cl_new = np.vstack(cl_new).T
+        '''
         cat_data =  chunk[["C"+str(i) for i in range(1,27)]]
         cat_data = cat_data.fillna('00000000')
 	cl = np.array(cat_data)
@@ -129,19 +111,23 @@ def processTrainFeatures(input_file, chunk_size, cat_test_stats, norm_features, 
 		else:
 		    cl_row.append(row[i])
             cl_new.append(cl_row)
+        '''  
 
         labels = pd.DataFrame(cl_new, columns=["C"+str(i) for i in range(1,27)])
 	#print labels
+
         norm_data = pd.concat([norm_data,labels], axis=1)
 
   	#Adding the time of day feature
-        time_of_day = pd.DataFrame(tod[tod_ptr:tod_ptr+chunk_size],columns=["Tod"])
-	tod_ptr += chunk_size
+        time_of_day = pd.DataFrame(tod[tod_ptr:tod_ptr+chunk_size_small],columns=["Tod"])
+	tod_ptr += chunk_size_small
         norm_data = pd.concat([norm_data, time_of_day], axis=1)
-
+ 
+        '''
         daily_temporal_mean = dtm_chunk[["Dtm"]]
         daily_temporal_mean = daily_temporal_mean.fillna(daily_temporal_mean.mean()) 
         norm_data = pd.concat([norm_data, daily_temporal_mean], axis=1)
+        '''
 
 	norm_data = norm_data.reset_index(drop=True) #Removes indexes from being printed in the csv file
 	#print norm_data	
@@ -152,11 +138,51 @@ def processTrainFeatures(input_file, chunk_size, cat_test_stats, norm_features, 
         count +=1
 	gc.collect()
 
-    #print stats,cat_stats
-    return stats,cat_stats;
+    return stats;
+
+def getTrainSetCategories(input_file, chunk_size):
+    cat_stats = []
+
+    for i in range(0,26):
+	frame = pd.DataFrame()
+	cat_stats.append(frame)
+
+    reader = pd.read_csv(input_file, chunksize=chunk_size)
+    count = 0 
+    for chunk in reader:
+	print 'Creating dictionary of catgorical features on training set:' + str(count * chunk_size)
+        #Generating histograms for categorical features
+	for i in range(0,26):
+	    column_name = "C"+ str(i+1)
+	    cat_view = chunk[['Label',column_name]]
+	    cframe = pd.DataFrame()
+	    cframe['category'] = cat_view.groupby(column_name).size().index
+	    cframe['value'] = cat_view.groupby(column_name).size().values
+            cat_stats[i] = pd.concat([cat_stats[i],cframe])
+        count +=1
+	gc.collect()
+
+    #Finish processing the categorical features	Aggregating stats over all chunks
+    for i in range(0,26):
+        cframe = pd.DataFrame()
+	cframe['category'] = cat_stats[i].groupby('category').sum().index
+	cframe['value'] = cat_stats[i].groupby('category').sum().values
+        cat_stats[i] = cframe
+    #Creating a dictionary for categories to find out unique ids in the test set
+    cat_new_stats = []
+    for i in range(0,26):
+        cl = np.array(cat_stats[i])
+        cat_dict = {cat[0]:int(cat[1]) for cat in cl}
+        cat_new_stats.append(cat_dict)
+        #print len(cat_dict.keys())
+        #print cat_dict
+    print "Finished building training set dictionary"
+
+    return cat_new_stats
+
 
 #@profile
-def processTestFeatures(input_file, chunk_size, int_stats, cat_train_stats, collapse_new_features, num_test_records):
+def processTestFeatures(input_file, chunk_size, output_file, int_stats, cat_train_stats, norm_features, collapse_new_features):
 
     '''
     inputs: 
@@ -168,33 +194,54 @@ def processTestFeatures(input_file, chunk_size, int_stats, cat_train_stats, coll
     returns
         -        
     '''
+    num_test_records = 6042136
+    print "Number of test records", num_test_records
+
     cat_stats = []
     for i in range(0,26):
 	frame = pd.DataFrame()
 	cat_stats.append(frame)
-  
-    print("Adding Time of Day and Daily Temporal Mean to test set")
-    tod = np.linspace(0,1,num_test_records)
-    tod_ptr = 0
-    print tod.shape
-  
-    reader = pd.read_csv(input_file, chunksize=chunk_size)
+ 
+    if norm_features == 1: 
+        print("Adding Time of Day and Daily Temporal Mean to test set")
+        tod = np.linspace(0,1,num_test_records)
+        tod_ptr = 0
+        print tod.shape
+
+    chunk_size_small = chunk_size/2 
+    reader = pd.read_csv(input_file, chunksize=chunk_size_small)
     count = 0 
     for chunk in reader:
-	print 'Processing test features:' + str(count * chunk_size)
+	print 'Processing test features:' + str(count * chunk_size_small)
         int_features = chunk[["I"+str(i) for i in range(1,14)]]
-        #Normalizing integer test statistics
-        norm_data = (int_features - int_stats['mean'])/int_stats['std']
-        norm_data = norm_data.fillna(0)
+
+        if norm_features == 1: 
+            #Normalizing integer test statistics
+            norm_data = (int_features - int_stats['mean'])/int_stats['std']
+            norm_data = norm_data.fillna(0)
+        else:
+            norm_data = int_features
         #print chunk['Id'].values 
         labels =  chunk[["Id"]]
         norm_data = pd.concat([labels, norm_data], axis=1)
-        cat_data =  chunk[["C"+str(i) for i in range(1,27)]]
-        cat_data = cat_data.fillna('00000000')
 
         if collapse_new_features == 1:
+            cl_new = []
+            for i in range(0,26): 
+                cat_data = chunk[["C"+str(i+1)]]
+                cat_data = cat_data.fillna('00000000')
+	        cl = np.array(cat_data)
+	        cl_col = []
+	        for row in cl:
+	            if row[0] not in cat_train_stats[i] and row[0] != '00000000':
+		        cl_col.append('ffffffff')
+		    else:
+		        cl_col.append(row[0])
+	        cl_new.append(np.asarray(cl_col))
+            cl_new = np.vstack(cl_new).T
+
+            '''
 	    cl = np.array(cat_data)
-            #print cl
             cl_new = []
             for row in cl:
                 cl_row = []
@@ -204,24 +251,30 @@ def processTestFeatures(input_file, chunk_size, int_stats, cat_train_stats, coll
 		    else:
 		        cl_row.append(row[i])
                 cl_new.append(cl_row)
+	    '''
 
             labels = pd.DataFrame(cl_new, columns=["C"+str(i) for i in range(1,27)])
 	    #print labels
         else:
-	    labels = cat_data
+	    labels = chunk[["C"+str(i) for i in range(1,27)]]
+            labels = labels.fillna('00000000')
         norm_data = pd.concat([norm_data,labels], axis=1)
 
-  	#Adding the time of day feature
-        time_of_day = pd.DataFrame(tod[tod_ptr:tod_ptr+chunk_size],columns=["Tod"])
-	tod_ptr += chunk_size
+        if norm_features == 1: 
+  	    #Adding the time of day feature
+            time_of_day = pd.DataFrame(tod[tod_ptr:tod_ptr+chunk_size_small],columns=["Tod"])
+	    tod_ptr += chunk_size_small
+        else:
+            time_of_day = chunk[["Tod"]]
         norm_data = pd.concat([norm_data, time_of_day], axis=1)
+            
 
 	norm_data = norm_data.reset_index(drop=True)
 	#print norm_data	
         if count == 0:
-            norm_data.to_csv('norm_test.csv',mode='a',header=True,index=False)
+            norm_data.to_csv(output_file,mode='a',header=True,index=False)
         else:
-            norm_data.to_csv('norm_test.csv',mode='a',header=False,index=False)
+            norm_data.to_csv(output_file,mode='a',header=False,index=False)
 
         count +=1
 	gc.collect()
@@ -262,7 +315,7 @@ def getTestSetCategories(input_file, chunk_size):
 	cat_new_stats.append(cat_dict)
     cat_stats = cat_new_stats
 
-    return cat_stats,(count*chunk_size)
+    return cat_stats
 
 def calculateOverlapTrainTest(cat_train_stats, cat_test_stats):
 
@@ -318,23 +371,28 @@ if __name__ == '__main__':
     #cfbpf.hashFeaturesToDenseMatrix()
     pp = PdfPages('catplots.pdf')
     training_file = "./norm.csv"
-    test_file = "./norm_test.csv"
+    test_file = "./norm_test_collapsed.csv"
 
-    if os.path.isfile(training_file):
-        os.remove(training_file)
     if os.path.isfile(test_file):
         os.remove(test_file)
+    st = []
+    cat_st = []
 
-    cat_test_stats, num_test_records = getTestSetCategories(sys.argv[2],int(sys.argv[3]))
-    print "Number of test records", num_test_records
 
+    if os.path.isfile(training_file) == False:
+        cat_test_stats = getTestSetCategories(sys.argv[2],int(sys.argv[3]))
 
-    stats, cat_train_stats = processTrainFeatures(sys.argv[1],int(sys.argv[3]),cat_test_stats,1,1)
-    print "Training data statistics "
-    print stats
+        stats = processTrainFeatures(sys.argv[1],int(sys.argv[3]),cat_test_stats)
+        print "Training data statistics "
+        print stats
+        processTestFeatures(sys.argv[2],int(sys.argv[3]),'norm_test.csv',stats,cat_st,1,0)
+    gc.collect()
 
-    processTestFeatures(sys.argv[2],int(sys.argv[3]),stats,cat_train_stats,1,num_test_records)
+    cat_train_stats = getTrainSetCategories(sys.argv[1],int(sys.argv[3]))
 
+    processTestFeatures('norm_test.csv',int(sys.argv[3]),'norm_test_collapsed.csv',st,cat_train_stats,0,1)
+
+    cat_test_stats = getTestSetCategories(sys.argv[2],int(sys.argv[3])/2)
     calculateOverlapTrainTest(cat_train_stats, cat_test_stats)
 
     print "Plotting distributions.... "
